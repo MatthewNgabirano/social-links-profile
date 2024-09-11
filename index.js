305383af268c9353fd30647c4c4bd967268b2038
MatthewNgabirano <91391196+MatthewNgabirano@users.noreply.github.com>
2024-09-12 00:29:36 +0300

    Add files via upload
    Any feedback is welcomed

---
+++ index.js
@@ -0,0 +1,11 @@
+const button1 = document.querySelector('#button1');
+const button2 = document.querySelector('#button2');
+const button3 = document.querySelector('#button3');
+const button4 = document.querySelector('#button4');
+const button5 = document.querySelector('#button5');
+
+button1.onclick = GitHub
+button2.onclick =Frontend-Mentor
+button3.onclick = linked
+button4.onclick =twitter
+button5.onclick =instagram
